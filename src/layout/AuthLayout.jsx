6c180f3a6d5b8599d11222a353cf5c338e745ce3
Asif-vai <mr3366533@gmail.com>
2025-11-17 12:28:43 +0600

    set up authentication layout

---
+++ src/layout/AuthLayout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Logo from "../components/Logo/Logo";
+import authImage from "../assets/authImage.png";
+import { Outlet } from "react-router";
+function AuthLayout() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div>
+        <Logo></Logo>
+      </div>
+      <div className="flex">
+        <div className="flex-1">
+          <Outlet></Outlet>
+        </div>
+        <div className="flex-1">
+          <img src={authImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AuthLayout;
---
+++ src/pages/Auth/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+      Reg
+    </div>
+  )
+}
+
+export default Register

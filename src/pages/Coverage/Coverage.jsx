49f5230880060685390ea0939a08b3c8e1189316
Asif-vai <mr3366533@gmail.com>
2025-11-17 11:30:20 +0600

    implement search and map

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useLoaderData } from "react-router";
@@ -7,6 +7,19 @@ function Coverage() {
   const position = [23.685, 90.3563];
   const services = useLoaderData();
   console.log(services);
+  const mapRef = useRef(null)
+  const handleSearch = (e) =>{
+    e.preventDefault()
+    const location = e.target.location.value;
+
+    const district = services.find(c => c.district.toLowerCase().includes(location.toLowerCase()));
+   
+    if(district){
+      const coord = [district.latitude,district.longitude];
+      mapRef.current.flyTo(coord,14)
+    }
+
+  }
 
   return (
     <div className="my-8 px-3 md:px-6">
@@ -15,13 +28,16 @@ function Coverage() {
           We are available in 64 districts
         </h1>
 
-        <div className="join p-3 flex justify-center">
-          <input
-            className="input join-item rounded-l-full outline-0 w-40 sm:w-60 md:w-80"
-            placeholder="Search here"
-          />
-          <button className="btn join-item rounded-r-full">Search</button>
-        </div>
+        <form onSubmit={handleSearch}>
+          <div className="join p-3 flex justify-center">
+            <input
+            name="location"
+              className="input join-item rounded-l-full outline-0 w-40 sm:w-60 md:w-80"
+              placeholder="Search here"
+            />
+            <button className="btn join-item rounded-r-full">Search</button>
+          </div>
+        </form>
       </div>
 
       <div className="w-full h-64 sm:h-80 md:h-[500px] lg:h-[650px] xl:h-[800px]  rounded-xl mt-6 overflow-hidden">
@@ -30,6 +46,7 @@ function Coverage() {
           zoom={7}
           scrollWheelZoom={false}
           className="w-full h-full"
+          ref={mapRef}
         >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
